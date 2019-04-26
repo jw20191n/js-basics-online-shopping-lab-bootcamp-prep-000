7a95c41a200b10e7881a610c77361c1a8ff8c811
Jiayi Wang <clairejw1015@gmail.com>
2019-04-26 00:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
+  itemPrice = Math.floor(Math.random() * 100) + 1
   function randomNumber(){
     return Math.floor(Math.random() * 100) + 1;
   }
