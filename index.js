f624480f61448af679f308c2807101c604aec039
Jiayi Wang <clairejw1015@gmail.com>
2019-04-26 00:33:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else{
     var all = total()
-    return "Your total cost is $" + all + ", which will be charged to the card " + cardNumber
+    return "Your total cost is $" + all + ", which will be charged to the card " + cardNumber + "."
   }
 }
