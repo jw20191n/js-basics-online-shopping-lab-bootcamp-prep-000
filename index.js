b50437a20c744ba4c5ddc26ca7a90d8e94c6b76f
Jiayi Wang <clairejw1015@gmail.com>
2019-04-26 00:37:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function setCart(c) {
 function addToCart(item) {
   var random = Math.floor(Math.random() * 100) + 1;
   
-  var newObject = {itemName:item, itemPrice:random}
+  var newObject = {itemName:item, itemPrice:random};
+  cart.push(newObject)
   
   return item + " has been added to your cart."
   
